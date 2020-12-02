bb35fe6f08c307bf11faa4e0b0306d226952addc
habex <habensf@gmail.com>
2020-12-02 12:50:33 -0800

    The Boogle Maps application stores the address assigned to a given vehicle based on latitude and longitude.

--- boogle-maps/src/main/java/com/udacity/boogle/maps/MockAddressRepository.java
+++ boogle-maps/src/main/java/com/udacity/boogle/maps/MockAddressRepository.java
@@ -45,7 +45,6 @@ class MockAddressRepository {
 
         String lat_lon = lat + "_" + lon;
         // Assign latitude and longitude to an Address
-
         if (addressMap.get(lat_lon)==null) {
             Address address = new Address();
             address = getRandom();
